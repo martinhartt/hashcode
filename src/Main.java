144094261c9a116b12f796223c1e13bd21d9d72a
zceeaba <zceeaba@ucl.ac.uk>
2017-02-23 20:17:42 +0000

    added code to test the sorting algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.ws.Endpoint;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,9 +15,28 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		for(i=0;i<10;i++)
+		{
+			Endpoint a[i]=new Endpoint(i,null);
+		}
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		
+		Map requestsend = new HashMap();
+		Map requestsendb= new HashMap();
+		Map requestsendc= new HashMap();
+        requestend.put(a[2],924);
+        requestend.put(a[9],837);
+        requestend.put(a[8],167);
+		Video vid=new Video(0,20,requestend);
+        requestendb.put(a[6],988);
+        requestendb.put(a[8],300);
+        requestendb.put(a[7],552);
+        Video vidb=new Video(1,11,requestendb);
+        requestendc.put(a[2],924);
+        requestendc.put(a[9],837);
+        requestendc.put(a[8],167);
+        Video vidc=new Video(2,50,requestend);
+
 		ArrayList<Endpoint> endpoints = new ArrayList<>();
 		ArrayList<Video> videos = new ArrayList<>();
 		
